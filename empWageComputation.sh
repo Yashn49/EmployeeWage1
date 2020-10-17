0c77164c53aa88b9ea143fcc255557a03d000f37
Yash <yashn49@gmail.com>
2020-10-17 14:04:01 +0530

    second commit

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 echo "Welcome To Employee Wage Computation Program"
+
+isPresent=0
+empCheck=$((RANDOM%2))
+
+if [ $empCheck -eq $isPresent ]
+then
+ echo "employee is present"
+else
+ echo "employee is absent"
+fi
