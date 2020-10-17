0b7642bb3e977d35bb0cfaee0405592c0dc01e58
Yash <yashn49@gmail.com>
2020-10-17 14:21:25 +0530

    third commit

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -11,3 +11,16 @@ then
 else
  echo "employee is absent"
 fi
+
+isPresent=0
+empCheck=$((RANDOM%2))
+WagePerHour=20
+Hours=8
+salary=$((WagePerHour*Hours))
+
+if [ $empCheck -eq $isPresent ]
+then
+ echo "Salary is : " $salary
+else
+ echo "Salary is 0"
+fi
