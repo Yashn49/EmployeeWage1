2a8f546aae4ae142a2cb2e6b21a326870458af4f
Yash <yashn49@gmail.com>
2020-10-18 13:53:49 +0530

    ninth commit

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -129,3 +129,70 @@ function empWageComputation(){
  echo "Salary is : " $salary
 }
 empWageComputation 8 16 0
+
+
+
+function empWageComputation(){
+ echo "Welcome to Employee Wage Computation Program"
+ isParttime=1
+ isFulltime=0
+ empCheck=$((RANDOM%3))
+ WagePerHour=20
+ DaysinMonth=$((RANDOM%32))
+ TotalWorkingHours=$((RANDOM%481))
+ if [ $DaysinMonth -ge 20 -o $TotalWorkingHours -ge 100 ]
+ then
+  case $empCheck in
+   $isParttime)
+          empHours=$1
+   ;;
+   $isFulltime)
+          empHours=$2
+   ;;
+   *)
+          empHours=$3
+   ;;
+  esac
+ fi
+ salary=$((DaysinMonth*WagePerHour*empHours))
+ echo "Salary is : " $salary
+declare -A dailyWage
+dailyWage[1]=$salary
+
+echo ${dailyWage[@]}
+echo ${#dailyWage[@]}
+}
+empWageComputation 8 16 0
+declare -A dailyWage
+dailyWage[1]=$salary
+dailyWage[2]=$salary
+dailyWage[3]=$salary
+dailyWage[4]=$salary
+dailyWage[5]=$salary
+dailyWage[6]=$salary
+dailyWage[7]=$salary
+dailyWage[8]=$salary
+dailyWage[9]=$salary
+dailyWage[10]=$salary
+dailyWage[11]=$salary
+dailyWage[12]=$salary
+dailyWage[13]=$salary
+dailyWage[14]=$salary
+dailyWage[15]=$salary
+dailyWage[16]=$salary
+dailyWage[17]=$salary
+dailyWage[18]=$salary
+dailyWage[19]=$salary
+dailyWage[20]=$salary
+dailyWage[21]=$salary
+dailyWage[22]=$salary
+dailyWage[23]=$salary
+dailyWage[24]=$salary
+dailyWage[25]=$salary
+dailyWage[26]=$salary
+dailyWage[27]=$salary
+dailyWage[28]=$salary
+dailyWage[29]=$salary
+dailyWage[30]=$salary
+echo "all keys" ${!dailyWage[@]}
+echo ${#dailyWage[@]}
